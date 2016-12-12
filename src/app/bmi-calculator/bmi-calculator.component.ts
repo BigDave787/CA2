f297e49165947a96539be1804c6304b89c09ba63
Enda Lee <enda.lee@ittdublin.ie>
2016-12-12 14:51:15 +0000

    calculations 2

--- src/app/bmi-calculator/bmi-calculator.component.ts
+++ src/app/bmi-calculator/bmi-calculator.component.ts
@@ -16,18 +16,43 @@ export class BmiCalculatorComponent implements OnInit {
   feet: number = null;
   inches: number = null;
   metres: number = null;
+  message: String = "hello";
 
   //methods/functions
+
+  //Main BMI calculation
+   calculateBmi() {
+    var weight = this.kgs
+    var height = this.metres
+
+    if(weight > 0 && height > 0){	
+    var finalBmi = weight/(height*height)
+
+    if(finalBmi < 18.5){
+    this.message = "Your estimated BMI is "+ finalBmi + " That you are too thin."
+    }
+
+    if(finalBmi > 18.5 && finalBmi <= 25){
+    this.message = "Your estimated BMI is "+ finalBmi + " That you are healthy."
+    }
+
+    if(finalBmi > 25){
+    this.message = "Your estimated BMI is "+ finalBmi + " That you have overweight."
+    }
+
+    }
+    }
+
 
   //weight conversions
   stoneConvert() {
     this.pounds = this.stone * 14.000;
-    this.kgs = this.stone/0.15747
+    this.kgs = this.stone / 0.15747
   }
 
   poundConvert() {
     this.stone = this.pounds * 0.071429;
-    this.kgs  = this.pounds/2.2046;
+    this.kgs  = this.pounds / 2.2046;
   }
 
   kgConvert() {
@@ -39,20 +64,20 @@ export class BmiCalculatorComponent implements OnInit {
 
   //height conversions
   feetConvert() {
-    this.inches = this.feet
-    this.metres = this.feet
+    this.inches = this.feet * 12.000;
+    this.metres = this.feet / 3.2808;
   }
   
 
   inchesConvert() {
-    this.feet = this.inches
-    this.metres = this.inches
+    this.feet = this.inches * 0.083333;
+    this.metres = this.inches / 39.370;
   }
 
 
   metresConvert() {
-    this.feet = this.metres
-    this.inches = this.metres
+    this.feet = this.metres * 3.2808;
+    this.inches = this.metres * 39.370;
   }
 
 
